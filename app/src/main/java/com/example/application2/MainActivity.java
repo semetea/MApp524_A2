12747782ae25ef119e4a068f30e761422f7888fa
semetea <55260856+semetea@users.noreply.github.com>
2022-09-29 17:04:44 -0400

    A2

---
+++ app/src/main/java/com/example/application2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.application2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] products;
+    ListView product_list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        product_list = (ListView) findViewById(R.id.product_list);
+        products = new String[] {"Pante", "Shoes", "Hats"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_row_layout, R.id.product_list, products);
+        product_list.setAdapter(adapter);
+    }
+}
